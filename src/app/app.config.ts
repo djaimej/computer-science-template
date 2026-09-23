628b1c20bf5f90dccc69599e1f1540f96bd47a92
djaimej <d.jaimej@hotmail.com>
2026-09-23 10:52:18 -0600

    [UPD]

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,32 +8,15 @@ import { provideMarkdown, MARKED_OPTIONS, KATEX_OPTIONS, MERMAID_OPTIONS } from 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withXhr(), withInterceptorsFromDi()),
     provideAngularSvgIcon(),
     provideMarkdown({
-      markedOptions: {
-        provide: MARKED_OPTIONS,
-        useValue: { gfm: true, breaks: false, pedantic: false }
-      },
-      katexOptions: {
-        provide: KATEX_OPTIONS,
-        useValue: {
-          displayMode: true,
-          throwOnError: false,
-          errorColor: '#cc0000',
-          macros: { "\\f": "#1f(#2)" },
-          strict: true
-        },
-      },
-      mermaidOptions: {
-        provide: MERMAID_OPTIONS,
-        useValue: {
-          fontFamily: 'PatrickHand',
-          elk: { mergeEdges: false, nodePlacementStrategy: 'NETWORK_SIMPLEX', cycleBreakingStrategy: 'MODEL_ORDER' }
-        }
-      }
+      markedOptions: { provide: MARKED_OPTIONS, useValue: { gfm: true, breaks: false, pedantic: false } },
+      katexOptions: { provide: KATEX_OPTIONS, useValue: { displayMode: true, throwOnError: false, errorColor: '#cc0000', macros: { "\\f": "#1f(#2)" }, strict: true } },
+      mermaidOptions: { provide: MERMAID_OPTIONS, useValue: { fontFamily: 'PatrickHand', elk: { mergeEdges: false, nodePlacementStrategy: 'NETWORK_SIMPLEX', cycleBreakingStrategy: 'MODEL_ORDER' } } },
     }),
   ]
 };
